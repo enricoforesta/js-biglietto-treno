828a42390d961c8a2ccd7b5548a3023f161bdab5
enricoforesta <enrico.foresta97@gmail.com>
2023-11-06 17:36:03 +0100

    feat: fine esercizio

--- js/main-js.js
+++ js/main-js.js
@@ -6,7 +6,7 @@
 
 const elementKm=parseInt(prompt("inserire i km da percorrere")); 
 
-console.log( "I kilometri sono:" , elementKm , "km");
+console.log( "I kilometri sono:" , elementKm.toFixed(2) , "km");
 
 /*
 2. Chiedere all'utente l'età, salvarlo in una variabile e trasformarlo in numero.
@@ -29,13 +29,13 @@ console.log("Età: ", elementEta);
 
 
 const elementSomma=(elementKm * 0.21);
-
+   
 
 if(elementEta < 18) {
 
     const elementSomma= ((elementKm * 0.21) * 0.8);
-
-    console.log("Somma con sconto 20% ", elementSomma, "€");
+    
+    console.log("Somma con sconto 20% ", elementSomma.toFixed(2), "€");
 
 }
 
@@ -43,12 +43,12 @@ else if(elementEta > 65){
 
     const elementSomma= ((elementKm * 0.21) * 0.6);
 
-    console.log("Somma con sconto 40% ", elementSomma, "€");
+    console.log("Somma con sconto 40% ", elementSomma.toFixed(2), "€");
 
 }
 
 else{
-    console.log("Somma", elementSomma, "€");
+    console.log("Somma", elementSomma.toFixed(2), "€");
 }
 
 
